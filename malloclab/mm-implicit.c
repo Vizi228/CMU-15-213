c5dc90027ca1315434dbca809d7f139f452f9f72
Myroslav <myroyelechko@gmail.com>
2024-02-29 18:23:48 +0100

    Solution for malloclab

--- malloclab/mm-implicit.c
+++ malloclab/mm-implicit.c
@@ -41,24 +41,10 @@ static void *extend_heap(size_t words);
 static void *find_fit(size_t size);
 static void place(void *bp, size_t size);
 
-static void free_push(void *bp);
-static void free_swap(void *bp);
-static void free_clear(void *bp);
-
-static void check_heap();
-
 // single word size, double word size and chunk size for heap extending
 #define WSIZE 4
 #define DSIZE 8
-#define MIN_BLOCKSIZE (DSIZE * 2)
 #define CHUNKSIZE (1<<12)
-/* single word (4) or double word (8) alignment */
-#define ALIGNMENT DSIZE
-
-/* rounds up to the nearest multiple of ALIGNMENT */
-#define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7)
-
-#define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
 #define MAX(x, y) ((x) > (y)? (x) : (y))
 
@@ -81,47 +67,22 @@ static void check_heap();
 #define NEXT_BLKP(p) ((char *)(p) + GET_SIZE((char *)(p) - WSIZE))
 #define PREV_BLKP(p) ((char *)(p) - GET_SIZE((char *)(p) - DSIZE))
 
-// get and put pointer to next and prev pointer in free blocks
-#define FREE_PREV(p) ((char *)(p))
-#define FREE_NEXT(p) ((char *)(p) + WSIZE)
-#define GET_PREV(p) (GET(FREE_PREV(p)))
-#define GET_NEXT(p) (GET(FREE_NEXT(p)))
-#define PUT_PREV(p, val) (PUT(FREE_PREV(p), val))
-#define PUT_NEXT(p, val) (PUT(FREE_NEXT(p), val))
-
-static char *heap_list;
-static char *head;
-static char *tail;
+static char *heap_listp;
 
 /* 
  * mm_init - initialize the malloc package.
  */
 int mm_init(void)
 {
-    char *brk;
-    if ((brk = mem_sbrk((MIN_BLOCKSIZE * 2) + DSIZE)) == (void *)-1) {
+    if ((heap_listp = mem_sbrk(4*WSIZE)) == (void *)-1) {
         return -1;
     }
 
-    // head
-    PUT(brk + (0*WSIZE), 0);
-    PUT(brk + (1*WSIZE), PACK(MIN_BLOCKSIZE, 1));
-    PUT(brk + (2*WSIZE), NULL);
-    PUT(brk + (3*WSIZE), NULL);
-    PUT(brk + (4*WSIZE), PACK(MIN_BLOCKSIZE, 1));
-
-    // tail
-    PUT(brk + (5*WSIZE), PACK(MIN_BLOCKSIZE, 1));
-    PUT(brk + (6*WSIZE), NULL);
-    PUT(brk + (7*WSIZE), NULL);
-    PUT(brk + (8*WSIZE), PACK(MIN_BLOCKSIZE, 1));
-
-    PUT(brk + (9*WSIZE), PACK(0, 1));
-    heap_list = brk + (2*WSIZE);
-    head = brk + (2*WSIZE);
-    tail = brk + (6*WSIZE);
-    PUT_PREV(tail, head);
-    PUT_NEXT(head, tail);
+    PUT(heap_listp, 0);
+    PUT(heap_listp + (1*WSIZE), PACK(DSIZE, 1));
+    PUT(heap_listp + (2*WSIZE), PACK(DSIZE, 1));
+    PUT(heap_listp + (3*WSIZE), PACK(0, 1));
+    heap_listp += (2 * WSIZE);
 
     if (extend_heap(CHUNKSIZE/WSIZE) == NULL) {
         return -1;
@@ -145,35 +106,7 @@ static void *extend_heap(size_t words)
     PUT(FTRP(bp), PACK(size, 0));
     PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1));
 
-    bp = coalesce(bp);
-    free_push(bp);
-    return bp;
-}
-
-/*
- * free_push - Push block to head.
- */
-static void free_push(void *bp) {
-    PUT_PREV(bp, head);
-    PUT_NEXT(bp, GET_NEXT(head));
-    PUT_PREV(GET_NEXT(bp), bp);
-    PUT_NEXT(head, bp);
-}
-
-/*
- * free_swap - Swap pointers in the block.
- */
-static void free_swap(void *bp) {
-    PUT_NEXT(GET_PREV(bp), GET_NEXT(bp));
-    PUT_PREV(GET_NEXT(bp), GET_PREV(bp));
-}
-
-/*
- * free_clear - Remove pointers.
- */
-static void free_clear(void *bp) {
-    PUT_NEXT(bp, NULL);
-    PUT_PREV(bp, NULL);
+    return coalesce(bp);
 }
 
 /* 
@@ -182,15 +115,21 @@ static void free_clear(void *bp) {
  */
 void *mm_malloc(size_t size)
 {
+    size_t asize, extendsize;
     char *bp;
-    size_t asize = ALIGN(MIN_BLOCKSIZE + size);
+
+    if (size <= DSIZE) {
+        asize = 2 * DSIZE;
+    } else {
+        asize = DSIZE * ((size + (DSIZE) + (DSIZE-1)) / DSIZE);
+    }
 
     if ((bp = find_fit(asize)) != NULL) {
         place(bp, asize);
         return bp;
     }
 
-    size_t extendsize = MAX(asize, CHUNKSIZE);
+    extendsize = MAX(asize, CHUNKSIZE);
     if ((bp = extend_heap(extendsize/WSIZE)) == NULL) {
         return NULL;
     }
@@ -203,8 +142,8 @@ void *mm_malloc(size_t size)
  */
 static void *find_fit(size_t size) {
     void *bp;
-    for(bp = GET(FREE_NEXT(head)); bp != NULL; bp = GET(FREE_NEXT(bp))) {
-        if (size <= GET_SIZE(HDRP(bp))) {
+    for(bp = heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp)) {
+        if (!GET_ALLOC(HDRP(bp)) && (size <= GET_SIZE(HDRP(bp)))) {
             return bp;
         }
     }
@@ -216,30 +155,15 @@ static void *find_fit(size_t size) {
  */
 static void place(void *bp, size_t asize) {
     size_t block_size = GET_SIZE(HDRP(bp));
-    if ((block_size - asize) > MIN_BLOCKSIZE) {
-        void *prev = GET(FREE_PREV(bp));
-        void *next = GET(FREE_NEXT(bp));
-        
+    if ((block_size - asize) > (DSIZE * 2)) {
         PUT(HDRP(bp), PACK(asize, 1));
         PUT(FTRP(bp), PACK(asize, 1));
-        free_clear(bp);
-
         bp = NEXT_BLKP(bp);
         PUT(HDRP(bp), PACK(block_size - asize, 0));
-        PUT(FREE_PREV(bp), prev);
-        PUT(FREE_NEXT(bp), next);
         PUT(FTRP(bp), PACK(block_size - asize, 0));
-
-        PUT(FREE_NEXT(prev), bp);
-        // it`s possible that we don`t have next element in the head
-        if (next != NULL) {
-            PUT(FREE_PREV(next), bp);
-        }
     } else {
-        free_swap(bp);
         PUT(HDRP(bp), PACK(block_size, 1));
         PUT(FTRP(bp), PACK(block_size, 1));
-        free_clear(bp);
     }
 }
 
@@ -251,8 +175,7 @@ void mm_free(void *ptr)
     size_t size = GET_SIZE(HDRP(ptr));
     PUT(HDRP(ptr), PACK(size, 0));
     PUT(FTRP(ptr), PACK(size, 0));
-    ptr = coalesce(ptr);
-    free_push(ptr);
+    coalesce(ptr);
 }
 
 /*
@@ -266,30 +189,19 @@ static void *coalesce(void *bp) {
     if (prev_alloc && next_alloc) {
         return bp;
     } else if (!prev_alloc && next_alloc) {
-        void *predecessor = PREV_BLKP(bp);
-        free_swap(predecessor);
-
-        size += GET_SIZE(HDRP(predecessor));
-        PUT(HDRP(predecessor), PACK(size, 0));
+        size += GET_SIZE(HDRP(PREV_BLKP(bp)));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
         PUT(FTRP(bp), PACK(size, 0));
-        bp = predecessor;
+        bp = PREV_BLKP(bp);
     } else if (prev_alloc && !next_alloc) {
-        void *successor = NEXT_BLKP(bp);
-        free_swap(successor);
-
-        size += GET_SIZE(HDRP(successor));
+        size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
         PUT(HDRP(bp), PACK(size, 0));
         PUT(FTRP(bp), PACK(size, 0));
     } else {
-        void *predecessor = PREV_BLKP(bp);
-        void *successor = NEXT_BLKP(bp);
-        free_swap(predecessor);
-        free_swap(successor);
-
-        size += GET_SIZE(HDRP(predecessor)) + GET_SIZE(FTRP(successor));
-        PUT(HDRP(predecessor), PACK(size, 0));
-        PUT(FTRP(successor), PACK(size, 0));
-        bp = predecessor;
+        size += GET_SIZE(HDRP(PREV_BLKP(bp))) + GET_SIZE(FTRP(NEXT_BLKP(bp)));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+        PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
+        bp = PREV_BLKP(bp);
     }
 
     return bp;
@@ -328,22 +240,4 @@ void *mm_realloc(void *ptr, size_t size)
     mm_free(oldptr);
 
     return newptr;
-}
-
-// Check consistency of the heap
-static void check_heap() {
-    unsigned int *bp;
-    for(bp = GET(FREE_NEXT(head)); bp != NULL; bp = GET(FREE_NEXT(bp))) {
-        if (bp != head && bp != tail) {
-            if (GET_ALLOC(HDRP(bp))) {
-                printf("Heap inconsistent!\nAllocated block(%x) in head!\n", bp);
-                exit(1);
-            }
-
-            if (GET_NEXT(bp) == NULL || GET_PREV(bp) == NULL) {
-                printf("Heap inconsistent!\nInvalid pointers in block(%x)!\n", bp);
-
-            }
-        }
-    }
 }
